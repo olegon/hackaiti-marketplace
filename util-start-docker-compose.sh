8b64734e596bb59274f3cfc567276520aed0206a
Leandro Gonçalves de Oliveira <olegon.main@gmail.com>
2020-03-21 16:35:20 -0300

    Adding script to start docker-compose

---
+++ util-start-docker-compose.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "$AWS_ACCESS_KEY_ID" ]
+then
+    echo "You must specify AWS_ACCESS_KEY_ID environment variable"
+    exit 1
+fi
+
+if [ -z "$AWS_SECRET_ACCESS_KEY" ]
+then
+    echo "You must specify AWS_SECRET_ACCESS_KEY environment variable"
+    exit 1
+fi
+
+if [ -z "$AWS_REGION" ]
+then
+    echo "You must specify AWS_REGION environment variable"
+    exit 1
+fi
+
+docker-compose up --build --force-recreate
